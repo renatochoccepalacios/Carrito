eacd79c852ba426eb38fa0e2813c36973cfe2353
renatochocce <renatochocce758@gmail.com>
2023-10-15 17:13:10 -0600

    hgfh

--- Carrito/app.js
+++
@@ -1,6 +0,0 @@
-const containerIcon = document.querySelector('.container-icon');
-const containerCart = document.querySelector('.container-cart-products');
-
-containerIcon.addEventListener('click', () => {
-    containerCart.classList.toggle('hidden-cart');
-})
